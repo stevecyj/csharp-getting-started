baab1928ad3e1cd48d7754440885fe0f2e02dfd4
stevecyj <ycsteven.tsao@gmail.com>
2023-01-30 15:25:58 +0800

    double quote

--- declaration-string/Program.cs
+++ declaration-string/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(string.IsNullOrWhiteSpace(value2));
 string value3="   ";
 Console.WriteLine(string.IsNullOrWhiteSpace(value3));*/
 
-string path="d:\\mydocument\\file.txt";
+/*string path="d:\\mydocument\\file.txt";
 Console.WriteLine(path);
 
 string path2=@"d:\mydocument\file.txt";
@@ -21,4 +21,10 @@ string query=@"SELECT Id,Name
 FROM Members
 WHERE Id>5 AND Id <100
 ORDER BY Name DESC";
-Console.WriteLine(query);
+Console.WriteLine(query);*/
+
+string value="thi is \"a\" book";
+Console.WriteLine(value);
+
+string value2 =@"this is ""a"" book";
+Console.WriteLine(value2);
