f21864ea0ac99ed8bc77afd0b8ab59c08e4e9d93
stevecyj <ycsteven.tsao@gmail.com>
2023-01-30 17:53:53 +0800

    TextInfo.ToTitleCase

---
+++ deal-with-string-pratice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+void DisplayHeader(string title)
+{
+    Console.WriteLine("\r\n");
+    Console.WriteLine(title);
+    Console.WriteLine(new string('=', 40));
+}
+
+void Q1()
+{
+    DisplayHeader("Q1");
+    string str = " abc ";
+    str=str.TrimStart();
+    str=str.TrimEnd();
+    Console.WriteLine(str.Length);
+}
+
+void Q2()
+{
+    DisplayHeader("Q2");
+    string str = "Allen Kuo";
+    Console.WriteLine(str.ToUpper());
+}
+
+void Q3()
+{
+    DisplayHeader("Q3");
+    string str = "aLLeN kUO";
+    TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+    Console.WriteLine("\"{0}\" to titlecase: {1}",str,myTI.ToTitleCase(str));
+}
+
+Q1();
+Q2();
+Q3();
