e9b447619f02ffd2eb98e5b13b7bb20b5dcf94cf
stevecyj <ycsteven.tsao@gmail.com>
2023-01-30 15:09:06 +0800

    wrap line, SQL

--- declaration-string/Program.cs
+++ declaration-string/Program.cs
@@ -1,4 +1,4 @@
-﻿string value1=null;
+﻿/*string value1=null;
 Console.WriteLine(string.IsNullOrEmpty(value1));
 
 string value2=string.Empty;
@@ -8,4 +8,17 @@ Console.WriteLine(string.IsNullOrWhiteSpace(value1));
 Console.WriteLine(string.IsNullOrWhiteSpace(value2));
 
 string value3="   ";
-Console.WriteLine(string.IsNullOrWhiteSpace(value3));
+Console.WriteLine(string.IsNullOrWhiteSpace(value3));*/
+
+string path="d:\\mydocument\\file.txt";
+Console.WriteLine(path);
+
+string path2=@"d:\mydocument\file.txt";
+Console.WriteLine(path2);
+
+// sql
+string query=@"SELECT Id,Name
+FROM Members
+WHERE Id>5 AND Id <100
+ORDER BY Name DESC";
+Console.WriteLine(query);
