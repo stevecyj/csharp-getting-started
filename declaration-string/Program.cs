a688cdcb4c38b500b8e7dc5e96a7890d09ff0681
stevecyj <ycsteven.tsao@gmail.com>
2023-01-30 15:00:30 +0800

    string null, empty, whitespace

---
+++ declaration-string/Program.cs
@@ -0,0 +1,11 @@
+﻿string value1=null;
+Console.WriteLine(string.IsNullOrEmpty(value1));
+
+string value2=string.Empty;
+Console.WriteLine(string.IsNullOrEmpty(value2));
+
+Console.WriteLine(string.IsNullOrWhiteSpace(value1));
+Console.WriteLine(string.IsNullOrWhiteSpace(value2));
+
+string value3="   ";
+Console.WriteLine(string.IsNullOrWhiteSpace(value3));
