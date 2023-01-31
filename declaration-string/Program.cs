3ba71380da1bb8f8b0c0451ede9bd949aceb4144
stevecyj <ycsteven.tsao@gmail.com>
2023-01-31 14:58:09 +0800

    add mark string

--- declaration-string/Program.cs
+++ declaration-string/Program.cs
@@ -1,14 +1,14 @@
-﻿/*string value1=null;
-Console.WriteLine(string.IsNullOrEmpty(value1));
+﻿string value1 = null;
+Console.WriteLine("null:" + string.IsNullOrEmpty(value1));
 
-string value2=string.Empty;
+var value2 = string.Empty;
 Console.WriteLine(string.IsNullOrEmpty(value2));
 
 Console.WriteLine(string.IsNullOrWhiteSpace(value1));
 Console.WriteLine(string.IsNullOrWhiteSpace(value2));
 
-string value3="   ";
-Console.WriteLine(string.IsNullOrWhiteSpace(value3));*/
+var value3 = "   ";
+Console.WriteLine(string.IsNullOrWhiteSpace(value3));
 
 /*string path="d:\\mydocument\\file.txt";
 Console.WriteLine(path);
@@ -23,8 +23,8 @@ WHERE Id>5 AND Id <100
 ORDER BY Name DESC";
 Console.WriteLine(query);*/
 
-string value="thi is \"a\" book";
+/*string value="thi is \"a\" book";
 Console.WriteLine(value);
 
 string value2 =@"this is ""a"" book";
-Console.WriteLine(value2);
+Console.WriteLine(value2);*/
